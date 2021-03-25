2b5b1bf7890080bf849071c309e6f2375fb4e385
Giorgio <giorgio.pignattari@gmail.it>
2021-03-25 18:06:28 +0100

    funzioni check e remove

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ var app = new Vue(
   {
     el: '#root',
     data: {
+      // array di todos
       todos: [
         {
           title: 'Allenamento',
@@ -23,11 +24,20 @@ var app = new Vue(
         {
           title: 'Pulire casa',
         },
-      ]
-
+      ],
+      // stringa vuota dell'input
+      textInput: '',
     },
     methods: {
-
+      // creo funzione per creare e aggiungere ToDo all'array todos
+      add: function(){
+        let obj = {
+          title: this.textInput
+        }
+        this.todos.push(obj);
+        this.textInput = '';
+      },
+      
     }
   }
 );
