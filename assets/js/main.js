209d449f5ce8c6b6693144781a0a838d128da454
Giorgio <giorgio.pignattari@gmail.it>
2021-03-25 16:43:10 +0100

    inizio html e css

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Descrizione
+// Rifare l'esercizio della todo list come fatto assieme in classe.
+// Bonus
+// Modificare un elemento della lista, ricordandosi di aggiornare il title di quello giusto
+
+var app = new Vue(
+  {
+    el: '#root',
+    data: {
+
+    },
+    methods: {
+
+    }
+  }
+);
